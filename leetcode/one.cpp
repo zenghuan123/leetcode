d6dea849c76631950aadd2ee9179c11f96a6bb21
曽欢 <3037450495@qq.com>
2018-06-21 11:16:51 +0800

    修改findMedianSortedArrays使其复杂度降低

--- leetcode/one.cpp
+++ leetcode/one.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 /*
 	vector<int> oneDay::Solution::twoSum(vector<int>& nums, int target) {
-			
+
 			(vector<int>::iterator it = nums.begin();
 			unordered_map<int, int> map;
 			int index = 0;
@@ -25,7 +25,7 @@
 				}
 				it++;
 			}
-		
+
 		}
 		*/
 
@@ -34,9 +34,9 @@ vector<int> one::Solution::twoSum(vector<int>& nums, int target) {
 	while (it != nums.end()) {
 		int subNum = target - *it;
 		vector<int>::iterator findResult = find(nums.begin(), nums.end(), subNum);
-		if (findResult != nums.end()&&findResult!=it)
+		if (findResult != nums.end() && findResult != it)
 		{
-			int i1=distance(nums.begin(), it);
+			int i1 = distance(nums.begin(), it);
 			int i2 = distance(nums.begin(), findResult);
 			vector<int> result;
 			result.push_back(i1);
@@ -77,11 +77,12 @@ one::ListNode* one::Solution::addTwoNumbers(one::ListNode* l1, one::ListNode* l2
 	first = sum;
 	l1 = l1->next;
 	l2 = l2->next;
-	while (l1 != NULL ||l2 != NULL) {
+	while (l1 != NULL || l2 != NULL) {
 		ListNode* sum = new ListNode(0);
 		if (l1 == NULL) {
 			sum->val = l2->val + carryBit;
-		}else if (l2 == NULL)
+		}
+		else if (l2 == NULL)
 		{
 			sum->val = l1->val + carryBit;
 		}
@@ -92,9 +93,9 @@ one::ListNode* one::Solution::addTwoNumbers(one::ListNode* l1, one::ListNode* l2
 		sum->val = sum->val % 10;
 		listNode->next = sum;
 		listNode = sum;
-		if(l1!=NULL)
+		if (l1 != NULL)
 			l1 = l1->next;
-		if(l2!=NULL)
+		if (l2 != NULL)
 			l2 = l2->next;
 	}
 	if (carryBit != 0) {
@@ -110,7 +111,7 @@ int one::Solution::lengthOfLongestSubstring(string s) {
 	unordered_set<char> set;
 	int result = 0;
 	int left = 0;
-	int right=0;
+	int right = 0;
 	while (left < s.size() && right < s.size()) {
 		if (set.find(s.at(right)) == set.end()) {
 			//没有找到
@@ -132,7 +133,17 @@ int one::Solution::lengthOfLongestSubstring(string s) {
 	}
 	return result;
 }
-double one::Solution::findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+double one::Solution::findMedianSortedArrays(vector<int>& n1, vector<int>& n2) {
+	//best 
+	//把两组数据分成相等大小的两份leftpart和rightpart 
+	//那么中间值在leftpart的最左边和right的最右边产生
+
+	//防止出现负数 j=mid-i; nums1.size必须小于等于nums2.size
+	
+
+
+	/*
+	* n的复杂度
 	vector<int>::iterator it1=nums1.begin();
 	vector<int>::iterator it2 = nums2.begin();
 	int midIndexLeft = (nums1.size() + nums2.size()+1) / 2;
@@ -164,7 +175,7 @@ double one::Solution::findMedianSortedArrays(vector<int>& nums1, vector<int>& nu
 			return ((double)i2 + (double)midLeft) / 2.0;
 		}
 		}
-	
 
+	*/
 
 }
