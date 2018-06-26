b201250e4f43b12961c1a7987c7a0556bb1b6bb6
曽欢 <3037450495@qq.com>
2018-06-26 14:01:05 +0800

    删除上次提交的错误文件

--- leetcode/one.cpp
+++ leetcode/one.cpp
@@ -396,39 +396,3 @@ bool one::Solution::match(string s, int startIndex, string simpleP) {
 	}
 	return true;
 }
-enum Result {
-	TRUE, FALSE
-};
-class Solution {
-	Result memo[][];
-
-	public bool isMatch(string text, string pattern) {
-		memo = new Result[text.length() + 1][pattern.length() + 1];
-		return dp(0, 0, text, pattern);
-	}
-
-	public boolean dp(int i, int j, String text, String pattern) {
-		if (memo[i][j] != null) {
-			return memo[i][j] == Result.TRUE;
-		}
-		boolean ans;
-		if (j == pattern.length()) {
-			ans = i == text.length();
-		}
-		else {
-			boolean first_match = (i < text.length() &&
-				(pattern.charAt(j) == text.charAt(i) ||
-					pattern.charAt(j) == '.'));
-
-			if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
-				ans = (dp(i, j + 2, text, pattern) ||
-					first_match && dp(i + 1, j, text, pattern));
-			}
-			else {
-				ans = first_match && dp(i + 1, j + 1, text, pattern);
-			}
-		}
-		memo[i][j] = ans ? Result.TRUE : Result.FALSE;
-		return ans;
-	}
-}
